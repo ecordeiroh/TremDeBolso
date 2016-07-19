ba25451d353ee970a8943cbb19fc1e4ac9b7e009
ecordeiroh <ecordeiroh@gmail.com>
2016-07-19 14:46:15 -0300

    asd

--- app/src/main/java/br/gov/cbtu/tremdebolso/MainActivity.java
+++ app/src/main/java/br/gov/cbtu/tremdebolso/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    // aqui é onde fica os image button
+    // aqui é onde fica os imagem button
     ImageButton bt, bt1, bt2, bt3;
     Intent i;
 
